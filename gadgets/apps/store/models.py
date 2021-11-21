0ddefef30850f51422174bf2f9f361f1a4538377
Dosu333 <vladamir1865@gmail.com>
2021-11-21 22:19:17 +0100

    Created product model

--- gadgets/apps/store/models.py
+++ gadgets/apps/store/models.py
@@ -15,4 +15,13 @@ class Category(models.Model):
     def __str__(self):
          return self.title
 
-         
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.title
