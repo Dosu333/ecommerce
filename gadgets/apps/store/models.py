9161187c39ceb1d3b39cc34f14a3a23f60a89c08
Dosu333 <vladamir1865@gmail.com>
2021-11-21 22:09:43 +0100

    Created category model

--- gadgets/apps/store/models.py
+++ gadgets/apps/store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+# class BaseModel(models.Model):
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+         return self.title
+
+         
