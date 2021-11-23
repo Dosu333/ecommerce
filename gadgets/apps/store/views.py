b685560fe09abff42781aa34a432086fa5171076
Dosu333 <vladamir1865@gmail.com>
2021-11-24 00:45:47 +0100

    create detail view for categories

--- gadgets/apps/store/views.py
+++ gadgets/apps/store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from  .models import Product
+from  .models import Product, Category
 
-def product_detail(request, slug):
+def product_detail(request, category_slug, slug):
     product = get_object_or_404(Product, slug=slug)
 
     context = {
@@ -9,3 +9,14 @@ def product_detail(request, slug):
     }
 
     return render(request, 'product_detail.html', context)
+
+def category_detail(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    products = category.products.all()
+
+    context = {
+        'category': category,
+        'products': products
+    }
+
+    return render(request, 'category_detail.html', context)
