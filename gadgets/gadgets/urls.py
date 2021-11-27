bb05478265f4a8817068cbe4c7a5f3e5c0ba4cc1
Dosu333 <vladamir1865@gmail.com>
2021-11-27 10:36:34 +0100

    Created Cart class

--- gadgets/gadgets/urls.py
+++ gadgets/gadgets/urls.py
@@ -18,12 +18,12 @@ from django.urls import path
 
 from apps.core.views import frontpage, contact, about
 from apps.store.views import product_detail, category_detail
-from apps.cart.views import cart
+from apps.cart.views import cart_detail
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('admin/', admin.site.urls),
-    path('cart/', cart, name='cart'),
+    path('cart/', cart_detail, name='cart'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
     path('<slug:category_slug>/<slug:slug>/', product_detail, name='product_detail'),
